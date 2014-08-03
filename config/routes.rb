9657693bfb5f6cd4667146a6c3c17d942ef6de08
vinodprakash <psvinodprakash@gmail.com>
2014-08-03 23:39:02 +0530

    Removing user controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
   
-  resources :users
-
   #devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
   get 'home/index' =>'home#index'
